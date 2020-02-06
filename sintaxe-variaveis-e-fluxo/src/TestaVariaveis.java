129b794b286e1ebb9728ae65126e49a2dbcb4b81
Pedro Henrique Freitas de Paula <phfedev@gmail.com>
2020-02-06 09:04:37 -0300

    Add files via upload

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,9 @@
+
+public class TestaVariaveis {
+
+	public static void main(String[] args) {
+		int idade = 20;
+		System.out.println("Olá novo teste "+idade);
+	}
+
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestePontoFlutuante.java
@@ -0,0 +1,10 @@
+
+public class TestePontoFlutuante {
+
+	public static void main(String[] args) {
+		double salario;
+		salario = 1260.30;
+		System.out.println("Salario: R$"+salario);
+	}
+
+}
